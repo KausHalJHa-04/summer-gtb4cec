789a5934cf2b55a1e7e6e719da82326cf02166bb
kaushal kumar jha <jhakaushal361@gmail.com>
2024-07-11 14:08:15 +0530

    second commit

---
+++ Week0/Day1/Operators.java
@@ -0,0 +1,27 @@
+package Day1;
+
+public class Operators {
+    public static void main(String[] args) {
+        
+    
+    
+            // int sum=0;
+            // for(int i=0;i<=25;i++){
+            //     sum=i+sum;
+            //     System.out.print(sum +" .. ");
+
+            // }
+        
+            int i=5;
+
+            while (i<15) {
+                if(
+                    i%2==0 && i%7==0)
+                System.out.println(i);
+                i++;
+                
+            }
+    }
+    }
+
+ 
--- Week0/homework.java
+++
@@ -1,6 +0,0 @@
-public class homework {
-
-    public static void main(String[] args) {
-        System.out.println("Today's Homework");
-    }
-}
